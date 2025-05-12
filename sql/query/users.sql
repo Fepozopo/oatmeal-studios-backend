c8cb0743572e66ea62daec5e247a3a1c486b0376
Shane DuPrey <36708583+Fepozopo@users.noreply.github.com>
2025-05-11 18:07:45 -0700

    Refactor user queries and schema: replace username with email in users table, update SELECT statements to specify fields

--- sql/query/users.sql
+++ sql/query/users.sql
@@ -1,32 +1,35 @@
 -- name: GetUser :one
-SELECT *
+SELECT id, created_at, updated_at, email, first_name, last_name
 FROM users
 WHERE id = $1;
 
 -- name: ListUsers :many
-SELECT *
+SELECT id, created_at, updated_at, email, first_name, last_name
 FROM users
 ORDER BY last_name, first_name;
 
 -- name: CreateUser :one
-INSERT INTO users (id, created_at, updated_at, email, password)
+INSERT INTO users (id, created_at, updated_at, email, password, first_name, last_name)
 VALUES (
     gen_random_uuid(),
     NOW(),
     NOW(),
     $1,
-    $2
+    $2,
+    $3,
+    $4
 )
-RETURNING *;
+RETURNING id, created_at, updated_at, email, first_name, last_name;
 
 -- name: UpdateUser :one
 UPDATE users
-SET first_name = $2,
-    last_name = $3,
-    password = $4,
+SET email = $2,
+    first_name = $3,
+    last_name = $4,
+    password = $5,
     updated_at = NOW()
 WHERE id = $1
-RETURNING id, created_at, updated_at, first_name, last_name;
+RETURNING id, created_at, updated_at, email, first_name, last_name;
 
 -- name: DeleteUser :exec
 DELETE FROM users
@@ -36,7 +39,7 @@ WHERE id = $1;
 DELETE FROM users;
 
 -- name: GetUserFromRefreshToken :one
-SELECT *
+SELECT users.id, users.created_at, users.updated_at, users.email, users.first_name, users.last_name
 FROM users
     INNER JOIN refresh_tokens ON users.id = refresh_tokens.user_id
 WHERE refresh_tokens.token = $1;
--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -3,7 +3,7 @@ CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    username TEXT UNIQUE NOT NULL,
+    email TEXT UNIQUE NOT NULL,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
     password TEXT NOT NULL
