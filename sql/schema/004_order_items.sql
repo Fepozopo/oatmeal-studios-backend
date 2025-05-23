00da8968aa90aee6aa1a0a02777af855d21fbecf
Shane DuPrey <36708583+Fepozopo@users.noreply.github.com>
2025-05-22 19:44:04 -0700

    Update default quantity in order_items table to 6

--- sql/schema/004_order_items.sql
+++ sql/schema/004_order_items.sql
@@ -3,7 +3,7 @@ CREATE TABLE order_items (
     id SERIAL PRIMARY KEY,
     order_id INTEGER NOT NULL REFERENCES orders(id) ON DELETE CASCADE,
     item TEXT NOT NULL,
-    quantity INTEGER NOT NULL DEFAULT 1,
+    quantity INTEGER NOT NULL DEFAULT 6,
     price FLOAT NOT NULL,
     discount FLOAT NOT NULL DEFAULT 0.0, -- Percentage discount on the item
     item_total FLOAT NOT NULL,
