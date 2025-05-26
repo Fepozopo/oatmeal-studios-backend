a586fe1e64aec732d8845bd638381db1a883971b
Shane DuPrey <36708583+Fepozopo@users.noreply.github.com>
2025-05-25 18:10:26 -0700

    Add ApiConfig struct and import necessary packages for configuration management

--- config/config.go
+++ config/config.go
@@ -1,3 +1,14 @@
 package config
 
-// Add configuration loading logic here
+import (
+	"sync/atomic"
+
+	"github.com/Fepozopo/oatmeal-studios-backend/internal/database"
+)
+
+type ApiConfig struct {
+	FileserverHits atomic.Int32
+	DbQueries      *database.Queries
+	Platform       string `env:"PLATFORM"`
+	TokenSecret    string `env:"TOKEN_SECRET"`
+}
