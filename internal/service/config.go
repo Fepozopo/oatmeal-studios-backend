f2ff8b12c0767d1a085d137cfb885ca148cd50e7
Shane DuPrey <36708583+Fepozopo@users.noreply.github.com>
2025-06-22 17:11:47 -0700

    Fix JSON field names for address in CreateCustomerInput struct

--- internal/service/config.go
+++ internal/service/config.go
@@ -35,8 +35,8 @@ type CreateCustomerInput struct {
 	ContactName  string  `json:"contact_name,omitempty"`
 	Email        string  `json:"email,omitempty"`
 	Phone        string  `json:"phone,omitempty"`
-	Address1     string  `json:"address1,omitempty"`
-	Address2     string  `json:"address2,omitempty"`
+	Address1     string  `json:"address_1,omitempty"`
+	Address2     string  `json:"address_2,omitempty"`
 	City         string  `json:"city,omitempty"`
 	State        string  `json:"state,omitempty"`
 	ZipCode      string  `json:"zip_code,omitempty"`
